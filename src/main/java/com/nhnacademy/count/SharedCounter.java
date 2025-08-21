a2c37a118b4b156f557daacfcba888ffab2b42f7
nhn-academy-marco <marco@ nhnacademy.com>
2025-08-21 15:50:11 +0900

    JavaDocs 추가 및 코드 주석 정리

--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -14,45 +14,82 @@ package com.nhnacademy.count;
 
 import java.util.concurrent.Semaphore;
 
+/**
+ * 여러 스레드에서 공유하여 사용하는 카운터 클래스
+ * 세마포어를 사용하여 스레드 안전성을 보장합니다.
+ */
 public class SharedCounter {
+    /** 현재 카운트 값 */
     private long count;
+
+    /** 스레드 동기화를 위한 세마포어 */
     private Semaphore semaphore;
 
-    public SharedCounter(){
-        count =0l;
+            /**
+             * 기본 생성자
+             * 카운터를 0으로 초기화합니다.
+             */
+            public SharedCounter() {
+        count = 0L;
     }
 
-    public SharedCounter(long count) {
-        if(count <0){
-            throw new IllegalArgumentException("count > 0 ");
+            /**
+             * 지정된 값으로 카운터를 초기화하는 생성자
+             *
+             * @param count 초기 카운트 값
+             * @throws IllegalArgumentException 카운트가 0 미만인 경우
+             */
+            public SharedCounter(long count) {
+        if(count < 0) {
+            throw new IllegalArgumentException("카운트는 0 이상이어야 합니다.");
         }
         this.count = count;
-        //TODO#1-1 semaphore를 생성 합니다.( 동시에 하나의 Thread만 접근할 수 있습니다. ), permits prameter를 확인하세요.
+        //TODO#1-1 세마포어를 생성합니다. (동시에 하나의 스레드만 접근할 수 있도록 permits 매개변수를 설정하세요.)
         semaphore = null;
     }
 
-    public long getCount(){
-        /*TODO#1-2 count 를 반환 합니다.
-            semaphore.acquire()를 호출하여 허가를 획득 합니다.
-            쓰레드가 작업이 완료되면
-            semaphore.release()를 호출하여
-            허가를 반환 합니다.
+            /**
+             * 현재 카운트 값을 반환합니다.
+             * 세마포어를 사용하여 스레드 안전성을 보장합니다.
+             *
+             * @return 현재 카운트 값
+             * @throws InterruptedException 스레드가 인터럽트된 경우
+             */
+            public long getCount() {
+        /*TODO#1-2 count를 반환합니다.
+            1. semaphore.acquire()를 호출하여 허가를 획득합니다.
+            2. 필요한 작업을 수행합니다.
+            3. 작업이 완료되면 semaphore.release()를 호출하여 허가를 반환합니다.
          */
 
         return count;
     }
 
-    public long increaseAndGet(){
-        /* TODO#1-3 count = count + 1 증가시키고 count를 반환 합니다.
-           1-2 처럼 semaphore를 이용해서 동기화할 수 있도록 구현 합니다.
+            /**
+             * 카운트 값을 1 증가시키고 증가된 값을 반환합니다.
+             * 세마포어를 사용하여 스레드 안전성을 보장합니다.
+             *
+             * @return 증가된 카운트 값
+             * @throws InterruptedException 스레드가 인터럽트된 경우
+             */
+            public long increaseAndGet() {
+        /* TODO#1-3 카운트를 1 증가시키고(count = count + 1) 증가된 값을 반환합니다.
+           1-2와 같이 세마포어를 이용하여 동기화를 구현합니다.
         */
         count = count + 1;
         return count;
     }
 
-    public long decreaseAndGet(){
-        /*TODO#1-4 count = count-1 감소시키고 count를 반환 합니다.
-          1-2 처럼 semaphore를 이용해서 동기화할 수 있도록 구현 합니다.
+            /**
+             * 카운트 값을 1 감소시키고 감소된 값을 반환합니다.
+             * 세마포어를 사용하여 스레드 안전성을 보장합니다.
+             *
+             * @return 감소된 카운트 값
+             * @throws InterruptedException 스레드가 인터럽트된 경우
+             */
+            public long decreaseAndGet() {
+        /*TODO#1-4 카운트를 1 감소시키고(count = count - 1) 감소된 값을 반환합니다.
+          1-2와 같이 세마포어를 이용하여 동기화를 구현합니다.
         */
         count = count - 1;
         return count;
--- src/main/java/com/nhnacademy/thread/CounterIncreaseHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterIncreaseHandler.java
@@ -17,30 +17,47 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.Objects;
 
+/**
+ * 공유 카운터를 주기적으로 증가시키는 작업을 수행하는 Runnable 구현체
+ * 인터럽트를 통한 작업 종료 처리를 지원합니다.
+ */
 @Slf4j
 public class CounterIncreaseHandler implements Runnable {
+    /** 스레드 간 공유되는 카운터 객체 */
     private final SharedCounter sharedCounter;
 
+    /**
+     * CounterIncreaseHandler 객체를 초기화합니다.
+     *
+     * @param sharedCounter 공유 카운터 객체
+     * @throws IllegalArgumentException sharedCounter가 null인 경우
+     */
     public CounterIncreaseHandler(SharedCounter sharedCounter) {
-        //sharedCounter를 초기화 합니다.  sharedCounter가 null 이면 IllegalArgumentException이 발생 합니다.
+        // sharedCounter를 초기화합니다. sharedCounter가 null이면 IllegalArgumentException을 발생시킵니다.
         if(Objects.isNull(sharedCounter)){
-            throw new IllegalArgumentException(String.format("SharedCount is null"));
+            throw new IllegalArgumentException("SharedCounter는 null일 수 없습니다.");
         }
         this.sharedCounter = sharedCounter;
     }
 
+    /**
+     * 스레드가 실행할 작업을 정의합니다.
+     * 1초마다 공유 카운터의 값을 증가시키고 로그를 출력합니다.
+     * 인터럽트가 발생하면 작업을 종료합니다.
+     */
     @Override
     public void run() {
-        //현제 Thread의 interrupted이 ture <--  while의 종료조건 : interrupt가 발생 했다면 종료 합니다.
+        // 현재 스레드의 interrupted 상태가 true가 되면 반복문을 종료합니다.
         while(!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(1000);
-                //sharedCounter의 count를 1증가 시키고 count값을 반환 합니다.
+                // sharedCounter의 카운트를 1 증가시키고 증가된 값을 반환받습니다.
                 long count = sharedCounter.increaseAndGet();
-                log.debug("thread:{}, count:{}", Thread.currentThread().getName(), count);
+                log.debug("스레드: {}, 카운트: {}", Thread.currentThread().getName(), count);
             } catch (Exception e) {
-                log.debug("{} - interrupt!",Thread.currentThread().getName());
-                //현제 Thread에 interrupt()를 호출하여 interrput()를 발생 시킵 니다. 즉 현제 Thread의 interrupted 값이 -> true로 변경 됩니다. -> 즉 while 문을 종료하게 됩니다.
+                log.debug("스레드: {} - 인터럽트 발생!", Thread.currentThread().getName());
+                // 현재 스레드에 interrupt()를 호출하여 인터럽트를 발생시킵니다.
+                // 이로 인해 스레드의 interrupted 상태가 true로 변경되어 while 문이 종료됩니다.
                 Thread.currentThread().interrupt();
             }
         }
