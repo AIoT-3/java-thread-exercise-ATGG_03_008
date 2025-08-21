4ddb2d64b8ff41e6aa476563a082630c558e4d8b
nhn-academy-marco <marco@ nhnacademy.com>
2025-08-21 13:29:30 +0900

    counter-thread 코드에 주석 추가 및 TODO 설명 구체화

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2024. NHN Academy Corp. All rights reserved.
+ * + Copyright 2025. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -14,26 +14,29 @@ package com.nhnacademy;
 
 import com.nhnacademy.thread.CounterHandler;
 
+/**
+ * 두 개의 스레드를 사용하여 하나의 CounterHandler 객체를 공유하는 예제 애플리케이션
+ */
 public class App
 {
     public static void main( String[] args )
     {
-        //TODO#1 CounterHandler 객체를 생성 합니다. countMaxSize : 10
+        //TODO#1 CounterHandler 객체를 생성합니다. 최대 카운트 값(countMaxSize)을 10으로 설정합니다.
         CounterHandler counterHandler;
 
-        //TODO#2 threadA 생성시 counterHandler 객체를 paramter로 전달 합니다.
+        //TODO#2 threadA 생성 시 counterHandler 객체를 생성자 매개변수로 전달합니다.
         Thread threadA;
 
-        //TODO#3 threadA의 name을 'my-counter-A' 로 설정 합니다.
+        //TODO#3 threadA의 이름을 'my-counter-A'로 설정합니다.
 
-        //TODO#4 threadB 생성시 counterHandler 객체를 paramter로 전달 합니다.
+        //TODO#4 threadB 생성 시 동일한 counterHandler 객체를 생성자 매개변수로 전달합니다.
         Thread threadB;
 
-        //TODO#5 threadB의 name을 'my-counter-B' 로 설정 합니다.
+        //TODO#5 threadB의 이름을 'my-counter-B'로 설정합니다.
 
-        //TODO#7 threadA를 시작 합니다.
+        //TODO#7 threadA의 start() 메소드를 호출하여 스레드를 실행합니다.
 
-        //TODO#8 threadB를 시작 합니다.
+        //TODO#8 threadB의 start() 메소드를 호출하여 스레드를 실행합니다.
 
     }
 }
--- src/main/java/com/nhnacademy/thread/CounterHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterHandler.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2024. NHN Academy Corp. All rights reserved.
+ * + Copyright 2025. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -14,12 +14,22 @@ package com.nhnacademy.thread;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 일정 시간 간격으로 카운트를 증가시키는 Runnable 구현체
+ * 여러 스레드에서 공유될 수 있는 카운터 핸들러 클래스
+ */
 @Slf4j
 public class CounterHandler implements Runnable  {
     private final long countMaxSize;
 
     private long count;
 
+    /**
+     * CounterHandler 객체를 초기화합니다.
+     *
+     * @param countMaxSize 카운터의 최대값
+     * @throws IllegalArgumentException countMaxSize가 0 이하인 경우
+     */
     public CounterHandler(long countMaxSize) {
         if(countMaxSize<=0){
             throw new IllegalArgumentException();
@@ -29,6 +39,10 @@ public class CounterHandler implements Runnable  {
         this.count=0l;
     }
 
+    /**
+     * 1초 간격으로 카운트를 증가시키고 현재 스레드 이름과 카운트 값을 로그로 출력합니다.
+     * 카운트가 지정된 최대값에 도달할 때까지 실행됩니다.
+     */
     @Override
     public void run() {
         do {
