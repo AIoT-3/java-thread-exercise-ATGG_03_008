0300dd0da3b762d78bf113fdad24277ee93ef4ff
nhn-academy-marco <marco@ nhnacademy.com>
2026-01-16 13:50:25 +0900

    copyright 및 코드 주석 수정

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -20,13 +20,13 @@ import com.nhnacademy.thread.Counter;
  */
 public class App 
 {
-    public static void main( String[] args ){
+    public static void main(String[] args) {
 
-        //TODO#6 현재 실행 중인 메인 스레드의 이름을 "my-thread"로 설정합니다.
-        //참고: 메인 스레드는 Thread.currentThread()로 접근할 수 있습니다.
+        // TODO#6 현재 실행 중인 메인 스레드의 이름을 "my-thread"로 설정합니다.
+        // 참고: 메인 스레드는 Thread.currentThread()로 접근할 수 있습니다.
         Thread main = Thread.currentThread();
 
-        //TODO#7 Counter 객체를 생성하고, countMaxSize 값을 10으로 설정한 후 run 메소드를 호출합니다.
+        // TODO#7 Counter 객체를 생성하고, countMaxSize 값을 10으로 설정한 후 run 메서드를 호출합니다.
 
     }
 }
--- src/main/java/com/nhnacademy/thread/Counter.java
+++ src/main/java/com/nhnacademy/thread/Counter.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -20,11 +20,11 @@ public class Counter {
     private long count;
 
     public Counter(long countMaxSize) {
-        //TODO#1 countMaxSize가 0보다 작으면 IllegalArgumentException 예외를 발생시킵니다.
+        // TODO#1 countMaxSize가 0보다 작으면 IllegalArgumentException 예외를 발생시킵니다.
 
-        //TODO#2 this.countMaxSize 필드를 초기화합니다.
+        // TODO#2 this.countMaxSize 필드를 초기화합니다.
 
-        //TODO#3 this.count 값을 0으로 초기화합니다.
+        // TODO#3 this.count 값을 0으로 초기화합니다.
 
     }
 
@@ -32,19 +32,19 @@ public class Counter {
 
         do {
 
-            /*TODO#4 1초 간격으로 count를 증가시킵니다.
-              Thread.sleep 메소드를 사용하여 스레드를 1초간 일시 중지시키세요.
+            /* TODO#4 1초 간격으로 count를 증가시킵니다.
+              Thread.sleep 메서드를 사용하여 스레드를 1초간 일시 중지시키세요.
               참고: https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/Thread.html#sleep(java.time.Duration)
             */
 
             count++;
 
-                         /*TODO#5 현재 카운트 값을 로그로 출력합니다.
+                         /* TODO#5 현재 카운트 값을 로그로 출력합니다.
                 출력 형식: "name:{스레드 이름}, count:{카운트 값}"
                 스레드 이름은 Thread.currentThread().getName()으로 가져옵니다.
                 예시: "name:my-thread, count:1"
              */
 
-        }while (count<countMaxSize);
+        } while (count < countMaxSize);
     }
 }
