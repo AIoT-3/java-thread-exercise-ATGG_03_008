17d7ca459677249a1652f7d6a94ea500f5c62a86
nhn-academy-marco <marco@ nhnacademy.com>
2026-01-16 13:59:28 +0900

    docs: 맞춤법, 띄어쓰기 및 용어 수정 (exercise-step1-4-counter-multi-thread)

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -19,24 +19,24 @@ import com.nhnacademy.thread.CounterHandler;
  */
 public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        //TODO#1 CounterHandler 객체를 생성합니다. 최대 카운트 값(countMaxSize)을 10으로 설정합니다.
+        // TODO #1 CounterHandler 객체를 생성합니다. 최대 카운트 값(countMaxSize)을 10으로 설정합니다.
         CounterHandler counterHandler;
 
-        //TODO#2 threadA 생성 시 counterHandler 객체를 생성자 매개변수로 전달합니다.
+        // TODO #2 threadA 생성 시 counterHandler 객체를 생성자 매개변수로 전달합니다.
         Thread threadA;
 
-        //TODO#3 threadA의 이름을 'my-counter-A'로 설정합니다.
+        // TODO #3 threadA의 이름을 'my-counter-A'로 설정합니다.
 
-        //TODO#4 threadB 생성 시 동일한 counterHandler 객체를 생성자 매개변수로 전달합니다.
+        // TODO #4 threadB 생성 시 동일한 counterHandler 객체를 생성자 매개변수로 전달합니다.
         Thread threadB;
 
-        //TODO#5 threadB의 이름을 'my-counter-B'로 설정합니다.
+        // TODO #5 threadB의 이름을 'my-counter-B'로 설정합니다.
 
-        //TODO#7 threadA의 start() 메소드를 호출하여 스레드를 실행합니다.
+        // TODO #7 threadA의 start() 메서드를 호출하여 스레드를 실행합니다.
 
-        //TODO#8 threadB의 start() 메소드를 호출하여 스레드를 실행합니다.
+        // TODO #8 threadB의 start() 메서드를 호출하여 스레드를 실행합니다.
 
     }
 }
