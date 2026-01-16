5e36c35eef405638e4c9100618f2ff4466d83f5f
nhn-academy-marco <marco@ nhnacademy.com>
2026-01-16 13:58:46 +0900

    docs: 맞춤법, 띄어쓰기 및 용어 수정 (exercise-step1-3-counter-by-runable)

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -19,18 +19,18 @@ import com.nhnacademy.thread.CounterHandler;
  */
 public class App
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        //TODO#4 CounterHandler 객체를 생성합니다. 최대 카운트 값(countMaxSize)을 10으로 설정합니다.
+        // TODO #4 CounterHandler 객체를 생성합니다. 최대 카운트 값(countMaxSize)을 10으로 설정합니다.
         CounterHandler counterHandler;
 
-        //TODO#5 Thread 객체 생성 시 counterHandler 객체를 생성자 매개변수로 전달합니다.
+        // TODO #5 Thread 객체 생성 시 counterHandler 객체를 생성자 매개변수로 전달합니다.
         Thread thread;
 
-        //TODO#6 생성된 thread의 이름을 "my-counter"로 설정합니다.
+        // TODO #6 생성된 thread의 이름을 "my-counter"로 설정합니다.
 
 
-        //TODO#7 thread의 start() 메소드를 호출하여 스레드를 실행합니다.
+        // TODO #7 thread의 start() 메서드를 호출하여 스레드를 실행합니다.
 
     }
 }
--- src/main/java/com/nhnacademy/thread/CounterHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterHandler.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -18,23 +18,23 @@ import lombok.extern.slf4j.Slf4j;
  * 일정 시간 간격으로 카운트를 증가시키는 Runnable 구현체
  */
 @Slf4j
-//TODO#1 Runnable 인터페이스를 구현하도록 수정합니다.
+// TODO #1 Runnable 인터페이스를 구현하도록 수정합니다.
 public class CounterHandler {
     private final long countMaxSize;
 
     private long count;
 
     public CounterHandler(long countMaxSize) {
-        //TODO#2 countMaxSize가 0 이하인 경우 IllegalArgumentException을 발생시킵니다.
+        // TODO #2 countMaxSize가 0 이하인 경우 IllegalArgumentException을 발생시킵니다.
 
 
         this.countMaxSize = countMaxSize;
-        this.count=0l;
+        this.count = 0L;
     }
 
     @Override
     public void run() {
-                 /* TODO#3 run 메소드 구현:
+                 /* TODO #3 run 메서드 구현:
              1. 1초 간격으로 카운트를 증가시키고 로그를 출력합니다.
              2. 카운트는 1부터 countMaxSize(10)까지 증가합니다.
              3. 출력 형식: "thread:[스레드명], count:[카운트값]"
@@ -43,6 +43,6 @@ public class CounterHandler {
 
         do {
 
-        }while (count<countMaxSize);
+        } while (count < countMaxSize);
     }
 }
