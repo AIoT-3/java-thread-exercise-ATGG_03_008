7aa06dc291174210c1d9273a978b48499560608c
nhn-academy-marco <marco@ nhnacademy.com>
2026-01-16 14:05:28 +0900

    docs: 맞춤법, 띄어쓰기 및 용어 수정 (exercise-step2-2-semaphore)

--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2024. NHN Academy Corp. All rights reserved.
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -40,11 +40,11 @@ public class SharedCounter {
              * @throws IllegalArgumentException 카운트가 0 미만인 경우
              */
             public SharedCounter(long count) {
-        if(count < 0) {
+        if (count < 0) {
             throw new IllegalArgumentException("카운트는 0 이상이어야 합니다.");
         }
         this.count = count;
-        //TODO#1-1 세마포어를 생성합니다. (동시에 하나의 스레드만 접근할 수 있도록 permits 매개변수를 설정하세요.)
+        // TODO #1-1 세마포어를 생성합니다. (동시에 하나의 스레드만 접근할 수 있도록 permits 매개변수를 설정하세요.)
         semaphore = null;
     }
 
@@ -56,7 +56,7 @@ public class SharedCounter {
              * @throws InterruptedException 스레드가 인터럽트된 경우
              */
             public long getCount() {
-        /*TODO#1-2 count를 반환합니다.
+        /* TODO #1-2 count를 반환합니다.
             1. semaphore.acquire()를 호출하여 허가를 획득합니다.
             2. 필요한 작업을 수행합니다.
             3. 작업이 완료되면 semaphore.release()를 호출하여 허가를 반환합니다.
@@ -73,7 +73,7 @@ public class SharedCounter {
              * @throws InterruptedException 스레드가 인터럽트된 경우
              */
             public long increaseAndGet() {
-        /* TODO#1-3 카운트를 1 증가시키고(count = count + 1) 증가된 값을 반환합니다.
+        /* TODO #1-3 카운트를 1 증가시키고(count = count + 1) 증가된 값을 반환합니다.
            1-2와 같이 세마포어를 이용하여 동기화를 구현합니다.
         */
         count = count + 1;
@@ -88,7 +88,7 @@ public class SharedCounter {
              * @throws InterruptedException 스레드가 인터럽트된 경우
              */
             public long decreaseAndGet() {
-        /*TODO#1-4 카운트를 1 감소시키고(count = count - 1) 감소된 값을 반환합니다.
+        /* TODO #1-4 카운트를 1 감소시키고(count = count - 1) 감소된 값을 반환합니다.
           1-2와 같이 세마포어를 이용하여 동기화를 구현합니다.
         */
         count = count - 1;
