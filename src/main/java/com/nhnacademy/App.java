c17bde7993d5d7ac02aed25dbb44028391cb4c53
nhn-academy-marco <marco@ nhnacademy.com>
2026-01-16 13:56:38 +0900

    docs: 맞춤법, 띄어쓰기 및 용어 수정 (exercise-step1-1-counter-by-main-thread)

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -22,11 +22,11 @@ public class App
 {
     public static void main(String[] args) {
 
-        // TODO#6 현재 실행 중인 메인 스레드의 이름을 "my-thread"로 설정합니다.
+        // TODO #6 현재 실행 중인 메인 스레드의 이름을 "my-thread"로 설정합니다.
         // 참고: 메인 스레드는 Thread.currentThread()로 접근할 수 있습니다.
         Thread main = Thread.currentThread();
 
-        // TODO#7 Counter 객체를 생성하고, countMaxSize 값을 10으로 설정한 후 run 메서드를 호출합니다.
+        // TODO #7 Counter 객체를 생성하고, countMaxSize 값을 10으로 설정한 후 run 메서드를 호출합니다.
 
     }
 }
--- src/main/java/com/nhnacademy/thread/Counter.java
+++ src/main/java/com/nhnacademy/thread/Counter.java
@@ -20,11 +20,11 @@ public class Counter {
     private long count;
 
     public Counter(long countMaxSize) {
-        // TODO#1 countMaxSize가 0보다 작으면 IllegalArgumentException 예외를 발생시킵니다.
+        // TODO #1 countMaxSize가 0보다 작으면 IllegalArgumentException 예외를 발생시킵니다.
 
-        // TODO#2 this.countMaxSize 필드를 초기화합니다.
+        // TODO #2 this.countMaxSize 필드를 초기화합니다.
 
-        // TODO#3 this.count 값을 0으로 초기화합니다.
+        // TODO #3 this.count 값을 0으로 초기화합니다.
 
     }
 
@@ -32,14 +32,14 @@ public class Counter {
 
         do {
 
-            /* TODO#4 1초 간격으로 count를 증가시킵니다.
+            /* TODO #4 1초 간격으로 count를 증가시킵니다.
               Thread.sleep 메서드를 사용하여 스레드를 1초간 일시 중지시키세요.
               참고: https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/Thread.html#sleep(java.time.Duration)
             */
 
             count++;
 
-                         /* TODO#5 현재 카운트 값을 로그로 출력합니다.
+                         /* TODO #5 현재 카운트 값을 로그로 출력합니다.
                 출력 형식: "name:{스레드 이름}, count:{카운트 값}"
                 스레드 이름은 Thread.currentThread().getName()으로 가져옵니다.
                 예시: "name:my-thread, count:1"
