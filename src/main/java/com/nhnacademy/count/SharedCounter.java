37cf0460b2f3582f4e29830a9af09eb2a35a26aa
nhn-academy-marco <marco@ nhnacademy.com>
2026-01-16 14:05:57 +0900

    docs: 맞춤법, 띄어쓰기 및 용어 수정 (exercise-step2-3-mutex)

--- src/main/java/com/nhnacademy/count/SharedCounter.java
+++ src/main/java/com/nhnacademy/count/SharedCounter.java
@@ -1,6 +1,6 @@
 /*
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + Copyright 2026. NHN Academy Corp. All rights reserved.
  * + * While every precaution has been taken in the preparation of this resource,  assumes no
  * + responsibility for errors or omissions, or for damages resulting from the use of the information
  * + contained herein
@@ -40,11 +40,11 @@ public class SharedCounter {
              * @throws IllegalArgumentException 카운트가 0 미만인 경우
              */
             public SharedCounter(long count) {
-        if(count < 0) {
+        if (count < 0) {
             throw new IllegalArgumentException("카운트는 0 이상이어야 합니다.");
         }
         this.count = count;
-                 /*TODO#1-1 ReentrantLock을 생성합니다. (mutex는 동시에 하나의 스레드만 접근할 수 있도록 합니다.)
+                 /* TODO #1-1 ReentrantLock을 생성합니다. (mutex는 동시에 하나의 스레드만 접근할 수 있도록 합니다.)
            ReentrantLock은 기본적으로 비공정한 락입니다. 공정성을 보장하도록 true 매개변수를 사용하여 초기화합니다.
          */
         mutex = null;
@@ -57,7 +57,7 @@ public class SharedCounter {
              * @return 현재 카운트 값
              */
             public long getCount() {
-        /*TODO#1-2 count를 반환합니다.
+        /* TODO #1-2 count를 반환합니다.
             1. mutex.lock()을 호출하여 다른 스레드가 접근할 수 없도록 락을 설정합니다.
             2. 필요한 작업을 수행합니다.
             3. 작업이 완료되면 mutex.unlock()을 호출하여 잠금을 해제합니다.
@@ -73,7 +73,7 @@ public class SharedCounter {
              * @return 증가된 카운트 값
              */
             public long increaseAndGet() {
-        /* TODO#1-3 카운트를 1 증가시키고(count = count + 1) 증가된 값을 반환합니다.
+        /* TODO #1-3 카운트를 1 증가시키고(count = count + 1) 증가된 값을 반환합니다.
            1-2와 같이 mutex를 이용하여 동기화를 구현합니다.
         */
         count = count + 1;
@@ -87,7 +87,7 @@ public class SharedCounter {
              * @return 감소된 카운트 값
              */
             public long decreaseAndGet() {
-        /*TODO#1-4 카운트를 1 감소시키고(count = count - 1) 감소된 값을 반환합니다.
+        /* TODO #1-4 카운트를 1 감소시키고(count = count - 1) 감소된 값을 반환합니다.
           1-2와 같이 mutex를 이용하여 동기화를 구현합니다.
         */
         count = count - 1;
